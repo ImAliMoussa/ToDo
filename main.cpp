f7bb9e04243fb1104e520766d299eb9a48a6a7b5
ImAliMoussa <38588200+ImAliMoussa@users.noreply.github.com>
2019-04-12 23:02:10 +0200

    Add notes feature, cleaned ui up a bit
    Notes still not saved though

--- main.cpp
+++ main.cpp
@@ -1,12 +1,20 @@
 #include "mainwindow.h"
 #include "view.h"
 #include <QApplication>
+#include <QFile>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     MainWindow *window = new MainWindow();
     View *view = new View(window);
+    QFile file(":/Stylesheets/styelsheet.qss");
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+       return 1;
+
+    QString StyleSheet = QLatin1String(file.readAll());
+
+    //qApp->setStyleSheet(StyleSheet);
     window->setView(view);
     window->setUpSlots();
     window->show();
